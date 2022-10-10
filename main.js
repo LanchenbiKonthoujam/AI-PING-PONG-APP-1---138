a4dc02e63ce5392a7909b3c5d31ea9b0fa0acbf8
LanchenbiKonthoujam <97613330+LanchenbiKonthoujam@users.noreply.github.com>
2022-10-10 20:41:11 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -25,13 +25,17 @@ var ball = {
 function setup(){
   var canvas =  createCanvas(700,600);
   canvas.parent("canvas");
+  video = createCapture(VIDEO);
+  video.size(700, 600);
+  video.hide();
+  poseNet = ml5.poseNet(video, modelLoaded);
 }
 
 
 function draw(){
 
  background(0); 
-
+ image(video, 0,0, 700, 600);
  fill("black");
  stroke("black");
  rect(680,0,20,700);
@@ -165,3 +169,6 @@ function paddleInCanvas(){
   }  
 }
 
+function modelLoaded(){
+    console.log("moadel_loaded");
+}
